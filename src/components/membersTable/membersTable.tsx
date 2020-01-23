0c6e81095ce6e8153f1a0822b669cff98b9098a4
alicia <aliciafdezrov@gmail.com>
2020-01-23 12:28:59 +0100

    Created a new entry on the state with the organization name and created an input to hold the organization name

--- src/components/membersTable/membersTable.tsx
+++ src/components/membersTable/membersTable.tsx
@@ -1,32 +1,39 @@
 import * as React from "react";
-import { MemberEntity } from "../../model/member";
-import { memberAPI } from "../../api/memberAPI";
-import { MemberRow } from "./memberRow";
-import { MemberHead } from "./memberHead";
+import {MemberEntity} from "../../model/member";
+import {memberAPI} from "../../api/memberAPI";
+import {MemberRow} from "./memberRow";
+import {MemberHead} from "./memberHead";
 
-interface Props {}
+interface Props {
+}
 
 export const MembersTableComponent = (props: Props) => {
-  const [members, setMembers] = React.useState<MemberEntity[]>([]);
+    const [members, setMembers] = React.useState<MemberEntity[]>([]);
+    const [organizationName, setOrganizationName] = React.useState<string>("lemoncode");
 
-  const loadMembers = () => {
-    memberAPI.getAllMembers("lemoncode").then(members => setMembers(members));
-  };
+    const loadMembers = () => {
+        memberAPI.getAllMembers("lemoncode").then(members => setMembers(members));
+    };
 
-  return (
-    <div className="row">
-      <h2> Members Page</h2>
-      <button onClick={loadMembers}>Load</button>
-      <table className="table">
-        <thead>
-          <MemberHead />
-        </thead>
-        <tbody>
-          {members.map((member: MemberEntity) => (
-            <MemberRow key={member.id} member={member} />
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+    return (
+        <div className="row">
+            <h2> Members Page</h2>
+            <div style={{
+                display: "inline-flex",
+            }}>
+                <button onClick={loadMembers}>Load</button>
+                <input type="text" value={organizationName}/>
+            </div>
+            <table className="table">
+                <thead>
+                <MemberHead/>
+                </thead>
+                <tbody>
+                {members.map((member: MemberEntity) => (
+                    <MemberRow key={member.id} member={member}/>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    );
 };
