98ced3d8da54e8c33bf0d1985b5b6bd3fdc61f48
alicia <aliciafdezrov@gmail.com>
2020-01-29 18:56:17 +0100

    Installed sass and sass-loader to handle scss files

--- webpack.config.js
+++ webpack.config.js
@@ -44,7 +44,29 @@ module.exports = {
         options: {
           name: "assets/img/[name].[ext]?[hash]"
         }
-      }
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: [
+          'style-loader', // creates style nodes from JS strings
+          {
+            loader: "css-loader", // translates CSS into CommonJS
+            options: {
+              modules: {
+                localIdentName: '[name]_[local]_[hash:base64:5]'
+              },
+              localsConvention: 'camelCase', //permite usar los estilos con camel case en vez de usando el nombre original de la regla de css
+            }
+          },
+          {
+            loader: "sass-loader", //translates SCSS into CSS
+            options: {
+              implementation: require("sass") //By default the sass-loader prefers node-sass. In order to avoid this situation you can use the implementation option
+            }
+          }
+        ]
+      },
     ]
   },
   plugins: [
