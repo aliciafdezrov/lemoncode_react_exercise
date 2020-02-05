fc9e0bd08a7a5273267c5356a9cfc3297da07971
alicia <aliciafdezrov@gmail.com>
2020-02-05 11:47:16 +0100

    Removed loader component from main file

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import {App} from './app';
-import {usePromiseTracker} from "react-promise-tracker";
-
-const LoadingIndicator = props => {
-    const {promiseInProgress} = usePromiseTracker();
-
-    return (
-        promiseInProgress &&
-        <h1>Hey some async call in progress ! </h1>
-    );
-};
 
 ReactDOM.render(
-    <>
-        <App/>
-        <LoadingIndicator/>
-    </>,
+    <App/>,
     document.getElementById('root')
 );
