7099c0b4e6fe4940164bf25453f8316822011c58
alicia <aliciafdezrov@gmail.com>
2020-02-06 19:43:17 +0100

    Obtained selected hotel to edit on hotel edit scene

--- hotel-collection-exercise/src/scenes/hotel-edit.scene.tsx
+++ hotel-collection-exercise/src/scenes/hotel-edit.scene.tsx
@@ -1,8 +1,13 @@
 import * as React from 'react';
 import {AppLayout} from 'layouts';
+import {useParams} from 'react-router-dom';
 
-export const HotelEditScene = () => (
-    <AppLayout>
-        <h1>Hotel Edit</h1>
-    </AppLayout>
-);
+export const HotelEditScene = () => {
+    const params: any = useParams();
+
+    return (
+        <AppLayout>
+            <h1>Hotel Edit: ${params.id}</h1>
+        </AppLayout>
+    );
+};
