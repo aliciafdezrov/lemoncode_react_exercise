ebc8b18d078e996f4909af4f71c8d99cdbccca92
alicia <aliciafdezrov@gmail.com>
2020-02-07 12:45:33 +0100

    Created first implementation for hotel edit component

--- hotel-collection-exercise/src/pods/hotel-edit/hotel-edit.component.tsx
+++ hotel-collection-exercise/src/pods/hotel-edit/hotel-edit.component.tsx
@@ -1,4 +1,12 @@
 import * as React from 'react';
+import InputLabel from "@material-ui/core/InputLabel";
+import TextField from "@material-ui/core/TextField";
+import {createStyles, Select, Theme} from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import {Rating} from "@material-ui/lab";
+import makeStyles from "@material-ui/core/styles/makeStyles";
+
+const classes = require('./hotel-edit.styles.scss');
 
 interface Props {
     hotelId: string;
@@ -8,8 +16,53 @@ export const HotelEditComponent: React.FunctionComponent<Props> = (props: Props)
     const {hotelId} = props;
 
     return (
-        <div>
-            <h1>HOTEL EDIIITTT: ${hotelId}</h1>
+        <div className={classes.container}>
+            <div className={classes.rowContainer}>
+                <div className={classes.inputLabel}>
+                    <InputLabel>Name</InputLabel>
+                </div>
+                <TextField fullWidth/>
+            </div>
+
+            <div className={classes.rowContainer}>
+                <img src={"https://material-ui.com/static/images/grid-list/breakfast.jpg"}
+                     alt={`Image for the hotel with id: ${hotelId}`}/>
+            </div>
+
+            <div className={classes.rowContainer}>
+                <div className={classes.inputLabel}>
+                    <InputLabel>Picture</InputLabel>
+                </div>
+                <TextField fullWidth/>
+            </div>
+
+            <div className={classes.rowContainer}>
+                <div className={classes.inputLabel}>
+                    <InputLabel>Rating</InputLabel>
+                </div>
+                <Rating
+                    name="hotel-rating"
+                />
+
+                <div className={classes.inputLabel}>
+                    <InputLabel>City</InputLabel>
+                </div>
+                <Select value={"Seattle"}/>
+            </div>
+
+            <div className={classes.rowContainer}>
+                <div className={classes.inputLabel}>
+                    <InputLabel>Description</InputLabel>
+                </div>
+                <TextField fullWidth
+                           variant="outlined"
+                           multiline
+                           rows="4"/>
+            </div>
+
+            <div className={classes.rowContainer}>
+                <Button variant="contained" color="primary" fullWidth>Save</Button>
+            </div>
         </div>
     );
 };
