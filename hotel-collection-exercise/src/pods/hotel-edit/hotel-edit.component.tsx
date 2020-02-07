5b10fe8e1a0b4add1a7922ea23e6beaebb59c63d
alicia <aliciafdezrov@gmail.com>
2020-02-07 13:07:47 +0100

    Added mocked data to showing hardcoded info on the form

--- hotel-collection-exercise/src/pods/hotel-edit/hotel-edit.component.tsx
+++ hotel-collection-exercise/src/pods/hotel-edit/hotel-edit.component.tsx
@@ -1,12 +1,21 @@
 import * as React from 'react';
 import InputLabel from "@material-ui/core/InputLabel";
 import TextField from "@material-ui/core/TextField";
-import {createStyles, Select, Theme} from "@material-ui/core";
+import {Select} from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import {Rating} from "@material-ui/lab";
-import makeStyles from "@material-ui/core/styles/makeStyles";
+import MenuItem from "@material-ui/core/MenuItem";
+import {HotelEntityVm} from "../hotel-collection/hotel-collection.vm";
 
 const classes = require('./hotel-edit.styles.scss');
+const mockHotel: HotelEntityVm = {
+    picture: "https://material-ui.com/static/images/grid-list/breakfast.jpg",
+    name: "Hotel BellaVista",
+    description: "Beautiful hotel located in Málaga",
+    rating: 4,
+    address: "Málaga",
+    id: "id-mocked",
+};
 
 interface Props {
     hotelId: string;
@@ -21,11 +30,11 @@ export const HotelEditComponent: React.FunctionComponent<Props> = (props: Props)
                 <div className={classes.inputLabel}>
                     <InputLabel>Name</InputLabel>
                 </div>
-                <TextField fullWidth/>
+                <TextField fullWidth value={mockHotel.name}/>
             </div>
 
             <div className={classes.rowContainer}>
-                <img src={"https://material-ui.com/static/images/grid-list/breakfast.jpg"}
+                <img src={mockHotel.picture}
                      alt={`Image for the hotel with id: ${hotelId}`}/>
             </div>
 
@@ -33,7 +42,7 @@ export const HotelEditComponent: React.FunctionComponent<Props> = (props: Props)
                 <div className={classes.inputLabel}>
                     <InputLabel>Picture</InputLabel>
                 </div>
-                <TextField fullWidth/>
+                <TextField fullWidth value={mockHotel.picture}/>
             </div>
 
             <div className={classes.rowContainer}>
@@ -42,12 +51,21 @@ export const HotelEditComponent: React.FunctionComponent<Props> = (props: Props)
                 </div>
                 <Rating
                     name="hotel-rating"
+                    value={mockHotel.rating}
                 />
 
                 <div className={classes.inputLabel}>
                     <InputLabel>City</InputLabel>
                 </div>
-                <Select value={"Seattle"}/>
+                <Select
+                    labelId="city-selector"
+                    id="city-selector-hotel-edit-component"
+                    value={0}
+                >
+                    <MenuItem value={0}>Málaga</MenuItem>
+                    <MenuItem value={1}>Granada</MenuItem>
+                    <MenuItem value={2}>Almería</MenuItem>
+                </Select>
             </div>
 
             <div className={classes.rowContainer}>
@@ -55,6 +73,7 @@ export const HotelEditComponent: React.FunctionComponent<Props> = (props: Props)
                     <InputLabel>Description</InputLabel>
                 </div>
                 <TextField fullWidth
+                           value={mockHotel.description}
                            variant="outlined"
                            multiline
                            rows="4"/>
