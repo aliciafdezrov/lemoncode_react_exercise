7b3f141c58bac0c039d9ac59f3aec48cd56c6203
alicia <aliciafdezrov@gmail.com>
2020-02-16 21:08:28 +0100

    Deleted mocked data and implemented the real hotel data retrieval (api)

--- hotel-collection-exercise/src/pods/hotel-edit/hotel-edit.container.tsx
+++ hotel-collection-exercise/src/pods/hotel-edit/hotel-edit.container.tsx
@@ -1,24 +1,36 @@
 import * as React from 'react';
 import {HotelEditComponent} from "./hotel-edit.component";
 import {HotelEntityVm} from "../hotel-collection/hotel-collection.vm";
+import {mapFromApiToVm} from "../hotel-collection/hotel-collection.mapper";
+import {getHotelById} from "./hotel-edit.api";
 
 interface Props {
     hotelId: string;
 }
 
 export const createMockedHotel = (): HotelEntityVm => ({
-    picture: "http://localhost:3000/thumbnails/50947_264_t.jpg",
-    name: "Hotel BellaVista",
-    description: "Beautiful hotel located in Málaga",
-    rating: 4,
-    address: "Málaga",
-    id: "id-mocked",
+    picture: "",
+    name: "",
+    description: "",
+    rating: 0,
+    address: "",
+    id: "",
 });
 
 export const HotelEditContainer = (props: Props) => {
     const {hotelId} = props;
 
     const [initialHotel, setHotel] = React.useState<HotelEntityVm>(createMockedHotel());
+
+    const loadHotel = () => {
+        getHotelById(hotelId).then(result =>
+            setHotel(mapFromApiToVm(result))
+        );
+    };
+
+    React.useEffect(() => {
+        loadHotel();
+    }, [hotelId]);
 
     const editHotel = (hotel, propId, newValue) => {
         let editedHotel = {...hotel};
