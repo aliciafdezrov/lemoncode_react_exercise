f2dac332901533b4c6888e62ece93016c18a3bfd
alicia <aliciafdezrov@gmail.com>
2020-02-16 19:09:47 +0100

    create a wrapper for combos, displaying form errors

---
+++ hotel-collection-exercise/src/common/components/forms/select-field.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react';
+import { FieldRenderProps } from 'react-final-form';
+import TextFieldMui from '@material-ui/core/TextField';
+import MenuItem from "@material-ui/core/MenuItem";
+
+interface Props extends FieldRenderProps<any, any> {}
+
+export const SelectField: React.FunctionComponent<Props> = props => {
+    const {
+        input: { name, onChange, value, ...restInput },
+        meta,
+        menuItemList,
+        ...rest
+    } = props;
+
+    const showError =
+        ((meta.submitError && !meta.dirtySinceLastSubmit) || meta.error) &&
+        meta.touched;
+
+    return (
+        <>
+            <TextFieldMui
+                {...rest}
+                name={name}
+                error={showError}
+                onChange={onChange}
+                value={value}
+                inputProps={restInput}
+                helperText={showError ? meta.error : ''}
+            >
+                {menuItemList.map(option => (
+                    <MenuItem key={option.value} value={option.value}>
+                        {option.label}
+                    </MenuItem>
+                ))}
+            </TextFieldMui>
+        </>
+    );
+};
